08384ea2473d465abc13c97852d36d3a60c51fd0
Oleg Krymskii <okrymsky@gmail.com>
2025-08-08 11:40:23 +0400

    Change deploy

--- src/app/media-list/media-list.component.ts
+++ src/app/media-list/media-list.component.ts
@@ -4,29 +4,29 @@ import { FilterOptions } from '../models/filter-options.model';
 import { FilterService } from '../services/filter.service';
 
 @Component({
-  selector: 'app-movie-list',
+  selector: 'app-media-list',
   standalone: false,
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  templateUrl: './media-list.component.html',
+  styleUrls: ['./media-list.component.css']
 })
 export class MovieListComponent {
   @Input() filters: FilterOptions | undefined;
-  @Input() set movies(value: Media[]) {
+  @Input() set medias(value: Media[]) {
     this.initMovies(value);
   }
-  medias: Media[] = [];
+  mediaCollection: Media[] = [];
 
   constructor(
     private filterService: FilterService
   ) {}
 
-  initMovies(movies: Media[]): void {
-    const sortedMovies = movies.sort((a, b) => {
+  initMovies(medias: Media[]): void {
+    const sortedMovies = medias.sort((a, b) => {
       const titleA = a.title.toLowerCase();
       const titleB = b.title.toLowerCase();      
       return titleA.localeCompare(titleB);
     });
-    this.medias = [...sortedMovies];
+    this.mediaCollection = [...sortedMovies];
     this.filterService.updateAvailableOptions({
       genres: this.filterService.getAvailableGenres(sortedMovies),
       years: this.filterService.getAvailableYears(sortedMovies)
@@ -34,7 +34,7 @@ export class MovieListComponent {
   }
 
   getFiltered(filters: FilterOptions | undefined): Media[] {
-    let result = [...this.medias];
+    let result = [...this.mediaCollection];
     if(!filters){
       return result;      
     }
